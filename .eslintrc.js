6908d84fd577d1d795a538a0ff58e185975fd427
Davis Ford <ford_davis@bah.com>
2018-03-20 13:20:09 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+# clean-text-utils
+### By Davis E Ford
+
+A collection of various text utilities that I've reached for and found lacking in Javascript.
